4359de59a4880e8d129066411e6f4f17444fcbdb
Priscr23 <Priscila.cervantes@iteso.mx>
2023-10-11 18:54:10 -0600

    1

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,29 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Diseña un programa para calcular el sueldo semanal de un empleado, dadas las horas trabajadas y la tarifa horaria (cuánto se le paga por cada hora trabajada). Considerar que si trabajó más de 48 horas, las horas excedentes se pagan al doble. Desglosar la cantidad y el importe correspondientes a las horas extras.
+750722
 """
 
+#Entradas
+horas_semanal = float(input("Ingrese horas trabajadas: "))
+tarifa_horaria = float(input("Ingrese la tarifa horaria: "))
 
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
+#Procesos
+horas_normales = 48
+horas_extras = horas_semanal - 48
+horas_extras_pago = tarifa_horaria * 2
+compensacion = horas_extras * horas_extras_pago
 
 
-# Salidas
-print(salida)
+sueldo_normal = horas_normales * tarifa_horaria
+pago_extra = compensacion + sueldo_normal
+
+
+#Salidas
+if horas_semanal <= 48:
+    print("Su sueldo es de: ", sueldo_normal)
+  
+
+elif horas_semanal > 48:
+    print("Su sueldo es: ", pago_extra)
+
+
